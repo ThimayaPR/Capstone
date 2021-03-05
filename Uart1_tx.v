e3b56e5f67a79a9937f0760dceb89bde68b759cf
ThimayaPR <65234516+ThimayaPR@users.noreply.github.com>
2021-03-06 00:11:21 +0530

    Serial-Communication
    UART Basic code

---
+++ Uart1_tx.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/05/2021 01:47:14 PM
+// Design Name: 
+// Module Name: Uart1_tx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Uart1_tx(
+    input clk,
+    input [7:0] data,
+    input transmit,
+    input reset,
+    output TxD
+    );
+endmodule
